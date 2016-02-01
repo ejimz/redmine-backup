2b8016a8a4a327524071186197a017d29caa078e
Enrique Jimenez <serialsito@gmail.com>
2016-02-01 14:29:09 +0100

    changes

--- redmine_backup.sh
+++ redmine_backup.sh
@@ -1,11 +1,18 @@
 #/bin/bash
 
-output_file=""
-path=""
 nfs="false"
-nfs_url="nfs.domain.com:/dir"
-nfs_mount_point="/mnt"
-nfs_dir="redmine"
+nfs_url="nfs.domain.com:/nfs"
+nfs_mount_point="/mnt/"
+backup_dir="/mnt/redmine"
+
+mysql_db="db"
+mysql_user="user"
+mysql_password="pw"
+
+app_dir="/var/redmine/current"
+
+cur_date=$(date +%d%m%y)
+cur_timestamp=$(date +%s)
 
 function show_help()
 {
@@ -30,6 +37,38 @@ function mount_nfs()
   fi
 }
 
+check_backup_dir()
+{
+  [ -d $backup_dir ] || mkdir -p $backup_dir
+  if [ $? -ne 0 ];then
+    echo "Problem checking backup dir. Exiting..."
+    exit 1
+  fi
+  mkdir -p $backup_dir/$cur_date
+}
+
+get_sql()
+{
+  mysqldump --opt --password=$mysql_password --user=$mysql_user $mysql_db > $backup_dir/$cur_date/$cur_timestamp.sql &> /dev/null
+  if [ $? -ne 0 ];then
+    echo "Problem getting mysql dump. Exiting..."
+    exit 1
+  else
+    echo "Mysql dump finished correctly."
+  fi
+}
+
+get_app_dir()
+{
+  cp -r $app_dir/ $backup_dir/$cur_date/$cur_timestamp > /dev/null
+  if [ $? -ne 0 ];then  
+    echo "Problem getting app dir. Exiting..."
+    exit 1
+  else
+    echo "App copied correctly."
+  fi
+}
+
 while getopts ":n" opt; do
   case $opt in
     n)
@@ -50,3 +89,6 @@ if [ "x$nfs" = "xtrue" ];then
   mount_nfs
 fi
 
+check_backup_dir
+get_sql
+get_app_dir
